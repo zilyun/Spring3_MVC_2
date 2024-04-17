d50592ccd269a56e7ee8dff4c268b2007d47305a
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-04-17 12:33:36 +0900

    Study: Spring MVC 2 교육 - 1

---
+++ src/main/java/com/naver/myhome/Spring3Mvc2Application.java
@@ -0,0 +1,13 @@
+package com.naver.myhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3Mvc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3Mvc2Application.class, args);
+	}
+
+}
